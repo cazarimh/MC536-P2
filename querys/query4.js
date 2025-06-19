c81ea9c4eb697e638a5b05f93418e032ea704b77
luizlenharo <lflenharo@gmail.com>
2025-06-19 20:50:46 -0300

    feat: add query4, query3Result and query4Result

---
+++ querys/query4.js
@@ -0,0 +1,61 @@
+db.emissao.aggregate([
+    {
+        $lookup: {
+            from: "estado",
+            localField: "localizacao.id_uf",
+            foreignField: "_id",
+            as: "estado"
+        }
+    },
+
+    { $unwind: "$estado" },
+    
+    {
+        $match: {
+            "ano_em": 2005,
+            "origem.tipo_origem": "Emissão",
+            "origem.setor_origem": "Agropecuária",
+            "estado.regiao.nome_reg": "SUDESTE"
+        }
+    },
+
+    {
+        $group: {
+            "_id": "$produto.nome_produto",
+            "qtd_em": { $sum: "$qtd_em" },
+        }
+    },
+
+    {
+        $group: {
+            "_id": null,
+            "produtos": {
+                $push: {
+                    "produto": "$_id",
+                    "qtd_em": "$qtd_em"
+                }
+            },
+            "total_em": { $sum: "$qtd_em" }
+        }
+    },
+
+    { $unwind: "$produtos" },
+
+    {
+        $project: {
+            "_id": 0,
+            "produto": "$produtos.produto",
+            "qtd_em": "$produtos.qtd_em",
+            "porcentagem": {
+                $round: [
+                    {$multiply: [{$divide: ["$produtos.qtd_em", "$total_em"]}, 100]},
+                    2
+                ]
+            }
+        }
+    },
+
+    { $sort: { "porcentagem": -1} },
+    { $limit: 5 }
+])
+   
