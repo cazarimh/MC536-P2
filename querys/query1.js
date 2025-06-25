38e7ef4a19e975029ed8b8954283c7b783539e59
Gustavo Rodrigues <gustavomrodriguesgr@gmail.com>
2025-06-24 22:38:37 -0300

    correção queries 1 e 2

--- querys/query1.js
+++ querys/query1.js
@@ -1,32 +1,51 @@
-var estado = db.estado.findOne({ nome_uf: "SÃO PAULO" });
-
-if (estado) {
-    var id_uf = estado._id;
-
-    var total_result = db.emissao.aggregate([
-        { $match: { ano_em: 2022, "localizacao.id_uf": id_uf } },
-        { $group: { _id: null, total_emissao: { $sum: "$qtd_em" } } }
-    ]).toArray();
-
-    var agro_result = db.emissao.aggregate([
-        { $match: { ano_em: 2022, "localizacao.id_uf": id_uf, "origem.setor_origem": "Agropecuária" } },
-        { $group: { _id: null, emitido_agro: { $sum: "$qtd_em" } } }
-    ]).toArray();
-
-    if (total_result.length > 0 && agro_result.length > 0) {
-        var total_emissao = total_result[0].total_emissao;
-        var emitido_agro = agro_result[0].emitido_agro;
-        var porcentagem = (emitido_agro * 100.0) / total_emissao;
-
-        printjson({
-            ano: 2022,
-            estado: "SÃO PAULO",
-            emitido_agro: emitido_agro,
-            porcentagem_sobre_total: porcentagem
-        });
-    } else {
-        print("Dados insuficientes para cálculo.");
+db.emissao.aggregate([
+  {
+    $match: {
+      ano_em: ano
     }
-} else {
-    print("Estado não encontrado.");
-}
+  },
+  {
+    $lookup: {
+      from: "estado",
+      localField: "localizacao.id_uf",
+      foreignField: "_id",
+      as: "estado"
+    }
+  },
+  { $unwind: "$estado" },
+  {
+    $match: {
+      "estado.nome_uf": "estado_nome"
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      total_emissao: { $sum: "$qtd_em" },
+      agro_emissao: {
+        $sum: {
+          $cond: [
+            { $eq: ["$origem.setor_origem", "Agropecuária"] },
+            "$qtd_em",
+            0
+          ]
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      estado: "estado_nome",
+      ano: ano,
+      emitido_agro: "$agro_emissao",
+      porcentagem_sobre_total: {
+        $cond: [
+          { $gt: ["$total_emissao", 0] },
+          { $multiply: [{ $divide: ["$agro_emissao", "$total_emissao"] }, 100] },
+          null
+        ]
+      }
+    }
+  }
+])
